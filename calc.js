c361d6b072a32bb24db623dab34928fc50dc5748
Athul PS <athulps336@gmail.com>
2023-04-12 17:20:05 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,12 @@
+function dispay(n){
+    result.value+=n
+}
+function Allclear(){
+    result.value=""
+}
+function evalExprsn(){
+    result.value=eval(result.value)
+}
+function erase(){
+    result.value=result.value.slice(0,-1)
+}
